ccddc127384a0c7df23309f8174d98c11a0cd752
AdityaParuleker <adityaparuleker@gmail.com>
2026-07-04 12:37:12 +0530

    Folder structure added

---
+++ commands.md
@@ -0,0 +1,1 @@
+sh template.sh
---
+++ template.sh
@@ -0,0 +1,17 @@
+
+#Creating directories for the project
+mkdir -p src
+mkdir -p research
+
+#Creating files for the project
+touch src/_init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory structure created successfully!"
